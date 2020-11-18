23e924ec26a7b72a6045d6a66ebb391ea1834c35
hansonho61004 <hansonho61004@gmail.com>
2020-11-18 23:33:38 +0800

    second build

---
+++ build/precache-manifest.8fb2887bdcab3849b7d628a66373b2c1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7415d77c52cbc7722b2308befbe77afb",
+    "url": "/online-pay/index.html"
+  },
+  {
+    "revision": "6bc9dc7fc4ef68484aba",
+    "url": "/online-pay/static/css/main.b6181a21.chunk.css"
+  },
+  {
+    "revision": "6f06a24492de13233e45",
+    "url": "/online-pay/static/js/2.c19d848f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/online-pay/static/js/2.c19d848f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc9dc7fc4ef68484aba",
+    "url": "/online-pay/static/js/main.e15df3f5.chunk.js"
+  },
+  {
+    "revision": "5a2bda73fe7415123a1b",
+    "url": "/online-pay/static/js/runtime-main.424dba17.js"
+  },
+  {
+    "revision": "25b3832d6366c70dce5ceb58cb0bf781",
+    "url": "/online-pay/static/media/atm.25b3832d.svg"
+  },
+  {
+    "revision": "00e64bdddcf7313d6ccd74d4386ce192",
+    "url": "/online-pay/static/media/back-three.00e64bdd.svg"
+  },
+  {
+    "revision": "7e5592dd52a934c8e08c96d6ce6aa701",
+    "url": "/online-pay/static/media/credit-card.7e5592dd.svg"
+  },
+  {
+    "revision": "0a142196ad99cc930c452acad4637a50",
+    "url": "/online-pay/static/media/finish.0a142196.svg"
+  },
+  {
+    "revision": "576d29f06b4d757b7b594968071cc0a5",
+    "url": "/online-pay/static/media/footer-background.576d29f0.svg"
+  },
+  {
+    "revision": "21a2a9c3218f80093697f0427a813200",
+    "url": "/online-pay/static/media/jcb.21a2a9c3.svg"
+  },
+  {
+    "revision": "dddb8410cfbeec642a0b81af4b748a9e",
+    "url": "/online-pay/static/media/mastercard.dddb8410.svg"
+  },
+  {
+    "revision": "9686ebda755ffc410142efa34f1c072b",
+    "url": "/online-pay/static/media/shop.9686ebda.svg"
+  },
+  {
+    "revision": "2844c06387f99bb11623135ad29d3f33",
+    "url": "/online-pay/static/media/unionpay.2844c063.svg"
+  },
+  {
+    "revision": "0821f75864f92ed97ab01e887ae027fb",
+    "url": "/online-pay/static/media/visa.0821f758.svg"
+  },
+  {
+    "revision": "7127170082fdd1f5c7ccf9c34e6e4864",
+    "url": "/online-pay/static/media/web-atm.71271700.svg"
+  }
+]);
